1fd645e5b503e84a94259f5ff1f3a75c87951970
jluu8080 <jasonluu1291@gmail.com>
2025-04-13 20:46:03 -0400

    Added Menu (Just a base)
    Definitely need to improve it later but it's just a base, all options are functional.
    
    Needs: Exception Handling & Improvements

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class overviewFunctions
 {
     static Scanner input = new Scanner(System.in); //Makes it accessible within the class.
+    static String emptyActivity = "\t--------"; //Sring Value used within weeklyOverview for empty activity/indexes
 
     public static void printWeeklyOverview(String[][] overview)
     {
@@ -41,12 +42,12 @@ class overviewFunctions
     }
 
     //Asks for hour input
-    public static int hourInput()
+    public static int timeInput()
     {
         //While true (infinite loop), asking for an hour continiously until the input is valid.
         while (true)
         {
-            //Asks for an hour from 1 AM - 12 AM??
+            //Asks for an hour from 12 AM - 11 PM
             System.out.print("What time (1-24): ");
             int hour = input.nextInt();
             
@@ -63,8 +64,92 @@ class overviewFunctions
             }
         }
     }
-}
 
+    //Adds Activity to the weekly overview
+    public static String addActivity()
+    {
+        input.nextLine(); //Buffer Line, used after right after doing a numerical/integer/double input
+        while (true)
+        {
+
+            System.out.print("What activity? (Ex. Running): ");
+            String activityInput = input.nextLine();//"ILoveAnimeGirlsAndICannotLie :P";//input.nextLine();
+
+            System.out.println();//Spacer
+
+            System.out.println("Your Activity is: '" + activityInput + "' Is this Correct?");
+            System.out.print("Yes or No?: ");
+            String userInput = input.nextLine();
+
+            //If the user says "No", then the loop will re-iterate/loop again. Otherwise, activityInput will be processed
+            //To corrrectly format and place itself into the weeklyOverview
+            if (userInput.equals("No") || !(userInput.equals("Yes")))
+            {
+                continue;
+            }
+
+
+            activityInput = "\t" + activityInput; //Adds a tabspace to it
+            /*
+            If activityInput is less than 8 characters, *\t aka tab space or ASCNI characters are considered 1 character not 2
+            Add spaces to fill it up to 8 or leave it be
+            Because it won't affect weeklyOverview formatting/spacing
+            */
+            if (activityInput.length() <=9)
+            {
+                for (int i = activityInput.length(); i<=9; i++)
+                {
+                    activityInput += " ";
+                }
+            }
+            //If greater than 8, cut down the string to 8 characters **Would need to format the entire overview with more spaces.
+            else
+            {
+                activityInput = activityInput.substring(0, 9); //Substring(0 - Inclusive, 9 - Exclusive) indexes of the string.
+                System.out.println(activityInput); //Prints the input for testing
+            }
+            return activityInput;
+        }
+    }
+
+    public static String[][] removeActivity(String[][] weeklyOverview)
+    {
+        int dayInput = 0;
+        int timeInput = 0;
+
+        System.out.println();//Spacer
+        overviewFunctions.printWeeklyOverview(weeklyOverview);
+
+        System.out.println("\n\nSelect the Day & Time of the activity, you wish to delete..."); //Double Spacer
+
+        dayInput = overviewFunctions.dayInput();
+        timeInput = overviewFunctions.timeInput();
+
+        weeklyOverview[timeInput][dayInput] = emptyActivity;
+        return weeklyOverview;
+
+    }
+
+
+public static String[][] replaceActivity(String[][] weeklyOverview)
+{
+
+    System.out.println();//Spacer
+    
+    overviewFunctions.printWeeklyOverview(weeklyOverview);
+
+    System.out.println("\n\n"); //Double Spacer
+
+    System.out.println("Select the Day & Time of the activity, you wish to delete...");
+    int dayInput = dayInput();
+    int timeInput = timeInput();
+    String newActivity = addActivity();
+
+    weeklyOverview[timeInput][dayInput] = newActivity;
+    return weeklyOverview;
+
+}
+}
 
 public class main 
 {
@@ -103,55 +188,61 @@ public class main
         };
 
 
-        /*
         System.out.println("Menu");
-        int dayInput = overviewFunctions.dayInput();
-
-        System.out.println();//Spacer
-
-
-        System.out.print("What time (1-24): ");
-        int timeInput = input.nextInt();
-
-        System.out.println();//Spacer
-
-        input.nextLine(); //Buffer line that must be used after asking for an Integer input and immediately a string input.
-
-        */
-        int timeInput = 2;
-        int dayInput = 2;
-
-        //System.out.print("What activity?: ");
-        String activityInput = "ILoveAnimeGirlsAndICannotLie :P";//input.nextLine();
-
-        System.out.println();//Spacer
+        System.out.println("(1) Add Activity");
+        System.out.println("(2) Delete Activity");
+        System.out.println("(3) Replace Activity\n");
         
-        activityInput = "\t" + activityInput; //Adds a tabspace to it
-        System.out.println(activityInput.length());
+        System.out.print("Select an Option(1-3): ");
+        String userInput = input.nextLine();
 
-        /*
-        If activityInput is less than 8 characters, *\t aka tab space or ASCNI characters are considered 1 character not 2
-        Add spaces to fill it up to 8 or leave it be
-        Because it won't affect weeklyOverview formatting/spacing
-        */
-        if (activityInput.length() <=9)
+
+        //Add an Activity
+        if (userInput.equals("1"))
         {
-            for (int i = activityInput.length(); i<=9; i++)
-            {
-                activityInput += " ";
-            }
+            int dayInput = overviewFunctions.dayInput();
+
+            int timeInput = overviewFunctions.timeInput();
+
+            String activityInput = overviewFunctions.addActivity();
+
+            weeklyOverview[timeInput][dayInput] = activityInput;
         }
-        //If greater than 8, cut down the string to 8 characters **Would need to format the entire overview with more spaces.
+        
+        //Remove an Activity
+        else if(userInput.equals("2"))
+        {
+            weeklyOverview[2][2] =  "\tSleeping"; //Used for Testing
+            weeklyOverview = overviewFunctions.removeActivity(weeklyOverview);
+        }
+
+        //Replace Activity
+        else if(userInput.equals("3"))
+        {
+            weeklyOverview = overviewFunctions.replaceActivity(weeklyOverview);
+        }
+
+        //Invalid Selection of Options/Invalid Input
         else
         {
-            activityInput = activityInput.substring(0, 9);
-            System.out.println(activityInput);
-        }
         
-        weeklyOverview[timeInput][dayInput] = activityInput; //Adds the activity to the day and hour the user inputted
+            System.out.println("Input Not Valid...(Pause Implemented)");
+            try
+            {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        //int timeInput = 2; //For Testing
+        //int dayInput = 2; //For Testing
+
         overviewFunctions.printWeeklyOverview(weeklyOverview); //Prints the entire Weekly Overview.
 
-        input.close();
+        input.close(); //Closes Input, to prevent Memory Leak
     }
     
 }
