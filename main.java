3b1ef52008d1bc494a26b104e3fe689e80c2be01
jluu8080 <jasonluu1291@gmail.com>
2025-04-14 21:09:35 -0400

    Added Base Code for Save File

--- main.java
+++ main.java
@@ -1,161 +1,14 @@
+//Reading a File Imports
+import java.io.File;
+import java.io.FileNotFoundException;
+
+//Writing to a File imports
+import java.io.FileWriter;
+import java.io.IOException;
+
+//Input & Reading a file import
 import java.util.Scanner;
 
-class overviewFunctions
-{
-    static Scanner input = new Scanner(System.in); //Makes it accessible within the class.
-    static String emptyActivity = "\t--------"; //Sring Value used within weeklyOverview for empty activity/indexes
-
-
-    //Prints the Weekly Overview (In its entirety)
-    public static void printWeeklyOverview(String[][] weeklyOverview)
-    {
-        //Prints the Weekly Overview
-        for (int i = 0; i<weeklyOverview.length;i++) //Why weeklyOverview.length is like this? Because it's an array
-        {
-            for (int j = 0; j<weeklyOverview[i].length;j++)
-            {
-                System.out.print(weeklyOverview[i][j]);
-            }
-        }
-    }
-
-    //Asks for Day Input
-    public static int dayInput()
-    {
-        //While true (infinite loop), asking for a day.
-        while (true)
-        {
-            //Asks for a day from Monday(1) - Sunday(7)
-            System.out.print("Enter Day (1-7): ");
-            int day = input.nextInt();
-            
-            System.out.println(); //Spacer
-            
-            //If day is greater than or equal 1 & less than 8
-            if (day>=1 && day<8)
-            {
-                return day; //Returns day
-            }
-            else
-            {
-                System.out.println("Your Input is invalid...\n"); //Error Message
-            }
-        }
-    }
-
-    //Asks for hour input
-    public static int timeInput()
-    {
-        //While true (infinite loop), asking for an hour continiously until the input is valid.
-        while (true)
-        {
-            //Asks for an hour from 12 AM - 11 PM
-            System.out.print("What time (1-24): ");
-            int hour = input.nextInt();
-            
-            System.out.println(); //Spacer
-            
-            //If day is greater than or equal 1 & less than 8
-            if (hour>=1 && hour<24)
-            {
-                return hour; //Returns hour and breaks out of loop because inputted value is valid.
-            }
-            else
-            {
-                System.out.println("Your Input is invalid...\n"); //Error Message
-            }
-        }
-    }
-
-    //Adds Activity to the weekly overview
-    public static String addActivity()
-    {
-        input.nextLine(); //Buffer Line, used after right after doing a numerical/integer/double input
-        while (true)
-        {
-
-            System.out.print("What activity? (Ex. Running): ");
-            String activityInput = input.nextLine();//"ILoveAnimeGirlsAndICannotLie :P";//input.nextLine(); used for testing
-
-            System.out.println();//Spacer
-
-            System.out.println("Your Activity is: '" + activityInput + "' Is this Correct?"); //Prints a message asking if this is what the user inputted
-            System.out.print("Yes or No?: ");
-            String userInput = input.nextLine(); //User Input for yes or no
-
-            //If the user says "No", then the loop will re-iterate/loop again.
-            if (userInput.equals("No") || !(userInput.equals("Yes")))
-            {
-                continue;
-            }
-
-            /*
-            Otherwise aka else, activityInput will be processed, adds a tabspace to correctly format itself
-            and \t aka tab space or ASCNI characters are considered as 1 character not 2.
-            */
-            activityInput = "\t" + activityInput; 
-
-
-            
-            /*
-            If activityInput is less than 9 characters because emptyActivty or "\t--------" is 9 characters in length
-            then spaces need to be added for it to correctly place and format itself inside weeklyOverview
-            */
-            if (activityInput.length() <=9)
-            {
-
-                for (int i = activityInput.length(); i<=9; i++) //why activityInput.length() is like that is because it's a String
-                {
-                    activityInput += " ";
-                }
-            }
-            //If greater than 8, cut down the string to 8 characters **Would need to format the entire overview with more spaces.
-            else
-            {
-                activityInput = activityInput.substring(0, 9); //Substring(0 - Inclusive, 9 - Exclusive) indexes of the string.
-                System.out.println(activityInput); //Prints the input for testing
-            }
-            return activityInput;
-        }
-    }
-
-    //Removes an activity from the weeklyOverview
-    public static String[][] removeActivity(String[][] weeklyOverview)
-    {
-        System.out.println();//Spacer
-        overviewFunctions.printWeeklyOverview(weeklyOverview); //Prints out the Weekly Overview to allow the user to see where and which activity to remove
-
-        System.out.println("\n\nSelect the Day & Time of the activity, you wish to delete..."); //Double Spacer
-
-        int dayInput = overviewFunctions.dayInput(); //Asks for the Day of the activity to be removed
-        int timeInput = overviewFunctions.timeInput(); //Asks for the hour of the activity to be removed.
-
-        weeklyOverview[timeInput][dayInput] = emptyActivity; //Deletes or in this case, replaces the desired activity to be deleted/replaced with "\t--------"
-        return weeklyOverview; //Returns the updated Weekly Overview
-
-    }
-
-    //Replaces an activity
-    public static String[][] replaceActivity(String[][] weeklyOverview)
-    {
-
-        System.out.println();//Spacer
-        
-        overviewFunctions.printWeeklyOverview(weeklyOverview); //Prints out the Weekly Overview to allow the user to see where and which activity to replace
-
-        System.out.println("\n\n"); //Double Spacer
-
-        System.out.println("Select the Day & Time of the activity, you wish to delete...");
-        int dayInput = dayInput();
-        int timeInput = timeInput();
-        //input.nextLine(); //Buffer Line; if Input gets fucked in this method, uncomment this to fix it
-        String newActivity = addActivity();
-
-        weeklyOverview[timeInput][dayInput] = newActivity; //Replaces the Activity at the user's desired day and hour.
-        return weeklyOverview;
-
-    }
-}
 
 public class main 
 {
@@ -193,65 +46,233 @@ public class main
 
         };
 
-
-        //Base Menu
-        System.out.println("Menu");
-        System.out.println("(1) Add Activity");
-        System.out.println("(2) Delete Activity");
-        System.out.println("(3) Replace Activity\n");
-        
-        System.out.print("Select an Option(1-3): ");
-        String userInput = input.nextLine();
-
-
-        //Add an Activity
-        if (userInput.equals("1"))
+        /*
+        //Reading a file
+        try
         {
-            int dayInput = overviewFunctions.dayInput();
+            File saved_schedule = new File("savedSchedule.txt");
+            String line;
+            Scanner reader = new Scanner(saved_schedule);
 
-            int timeInput = overviewFunctions.timeInput();
-
-            String activityInput = overviewFunctions.addActivity();
-
-            weeklyOverview[timeInput][dayInput] = activityInput;
-        }
-        
-        //Remove an Activity
-        else if(userInput.equals("2"))
-        {
-            weeklyOverview[2][2] =  "\tSleeping"; //Used for Testing, comment if need be
-            weeklyOverview = overviewFunctions.removeActivity(weeklyOverview);
-        }
-
-        //Replaces Activity
-        else if(userInput.equals("3"))
-        {
-            weeklyOverview = overviewFunctions.replaceActivity(weeklyOverview);
-        }
-
-        //Invalid Selection of Options/Invalid Input.
-        else
-        {
-        
-            System.out.println("Input Not Valid...(Pause Implemented)");
-
-            //Sleep/Pause Screen code for Java, this needs to be done in a try & catch | half-assed explanation, I sowry rushing comments out rn
-            try
+            while (reader.hasNextLine())
             {
-                Thread.sleep(2000); //Pauses for 2 seconds/2000 Miliseconds, throws a sleep exception
+                line = reader.nextLine();
+                System.out.println(line);
             }
-            catch (InterruptedException e) //Sleep exception caught
-            {
-                e.printStackTrace(); //Executes sleep/pause of the screen for the set amount of time
-            }
+            reader.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File Not Found...");
+            e.printStackTrace();
         }
 
-        //int timeInput = 2; //For Testing
-        //int dayInput = 2; //For Testing
+        //Writing to a file
+        try
+        {
+            FileWriter writer = new FileWriter("savedSchedule.txt"); //Overwrite Mode
+            FileWriter writer = new FileWriter("schedule.txt", true); //Append Mode
 
-        overviewFunctions.printWeeklyOverview(weeklyOverview); //Prints the entire Weekly Overview.
+            writer.write("Stupid Fuck!\n");
+
+            writer.close();
+            System.out.println("File Written Successfully! ");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Unable to write to file...");
+            e.printStackTrace();
+        }
+        */
+
+        /*
+        while (true)
+        {
+            //Base Menu
+            System.out.println("\n\nMenu");
+            System.out.println("(1) Add Activity");
+            System.out.println("(2) Delete Activity");
+            System.out.println("(3) Replace Activity");
+            System.out.println("(4) Quit\n");
+            
+            System.out.print("Select an Option(1 - 4): ");
+            String userInput = input.nextLine();
+            System.out.println();
+
+
+            //Add an Activity
+            if (userInput.equals("1"))
+            {
+                int dayInput = overviewFunctions.dayInput();
+
+                int timeInput = overviewFunctions.timeInput();
+
+                String activityInput = overviewFunctions.addActivity();
+
+                if(dayInput==7)
+                {
+                    activityInput +="\n";
+                }
+
+                weeklyOverview[timeInput][dayInput] = activityInput;
+            }
+            
+            //Remove an Activity
+            else if(userInput.equals("2"))
+            {
+                weeklyOverview[2][2] =  "\tSleeping"; //Used for Testing, comment if need be
+                weeklyOverview = overviewFunctions.removeActivity(weeklyOverview);
+            }
+
+            //Replaces Activity
+            else if(userInput.equals("3"))
+            {
+                weeklyOverview = overviewFunctions.replaceActivity(weeklyOverview);
+            }
+
+            else if (userInput.equals("4"))
+            {
+                break;
+            }
+
+            //Invalid Selection of Options/Invalid Input.
+            else
+            {
+            
+                System.out.println("Input Not Valid...(Pause Implemented)");
+
+                //Sleep/Pause Screen code for Java, this needs to be done in a try & catch | half-assed explanation, I sowry rushing comments out rn
+                try
+                {
+                    Thread.sleep(2000); //Pauses for 2 seconds/2000 Miliseconds, throws a sleep exception
+                }
+                catch (InterruptedException e) //Sleep exception caught
+                {
+                    e.printStackTrace(); //Executes sleep/pause of the screen for the set amount of time
+                }
+            }
+
+            //int timeInput = 2; //For Testing
+            //int dayInput = 2; //For Testing
+
+            //Clears the Screen
+            //System.out.print("\033[H\033[2J"); //Clears the Screen
+            //System.out.flush(); //Outputs it immediately
+            overviewFunctions.printWeeklyOverview(weeklyOverview); //Prints the entire Weekly Overview.
+        }
+        */
+        
+        //Clears the Screen
+        //System.out.print("\033[H\033[2J");
+        //System.out.flush();
+
+        //End of the Loop
+        //overviewFunctions.printWeeklyOverview(weeklyOverview); //Uncomment later after testing reaidng & writing
+
+        //Saving the Weekly Overview to savedSchedule(Computer-View).txt
+        try
+        {
+            FileWriter writer = new FileWriter("savedSchedule(Computer-View).txt"); //Overwrite Mode
+            String computerFileLine = "";
+            //FileWriter writer = new FileWriter("schedule.txt", true); //Append Mode
+
+            
+            for (int i = 0; i<weeklyOverview.length; i++)
+            {
+                for (int j = 0; j< weeklyOverview[i].length; j++)
+                {
+                    if (weeklyOverview[i][j].indexOf("\n") != -1)
+                    {
+                        computerFileLine = weeklyOverview[i][j].replace("\n","<br>"); //<br> DOES ABSOLUTELY NOTHING! in the .txt files it's just there as an indicator/a string for a line break/new row
+                    }
+                    else
+                    {
+                        computerFileLine = weeklyOverview[i][j] + "||"; // "||" DOES ABSOLUTELY NOTHING! In the .txt files, it's just there as an indictator/a string to indicate a cell & separate cells
+                    }
+                    writer.write(computerFileLine); //Adds line to File
+                }
+            }
+
+
+            writer.close(); //Closes file
+            System.out.println("File Written Successfully! ");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Unable to write to file...");
+            e.printStackTrace();
+        }
+
+        
+        //Saving the Weekly Overview to savedSchedule(User-View).txt
+        try
+        {
+            FileWriter writer1 = new FileWriter("savedSchedule(User-View).txt"); //Overwrite Mode
+            String userFileLine = "";
+            //FileWriter writer = new FileWriter("schedule.txt", true); //Append Mode
+
+            
+            for (int i = 0; i<weeklyOverview.length; i++)
+            {
+                for (int j = 0; j< weeklyOverview[i].length; j++)
+                {
+
+                    //<br> DOES ABSOLUTELY NOTHING!, in the .txt it's just there as an indicator/a string for a line break
+                    //While <br> does nothing, "\n" does make put everything on a new line in the .txt file
+                    userFileLine = weeklyOverview[i][j].replace("\n","<br>\n"); //For every "\n" that exists in an index value, will be replaced with "<br>\n"
+                    writer1.write(userFileLine); //Writes the Line 
+                }
+            }
+
+
+            writer1.close(); //Closes File
+            System.out.println("File Written Successfully! ");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Unable to write to file...");
+            e.printStackTrace();
+        }
+
+
+
+
+
+        //Reading WeeklyOverview(Computer's Version)
+        try
+        {
+            File saved_schedule = new File("savedSchedule(Computer-View).txt");
+            String line;
+            Scanner reader = new Scanner(saved_schedule);
+
+            while (reader.hasNextLine())
+            {
+                line = reader.nextLine(); //Stores each line into a String
+
+                /*Splits the line by "||" and turns them into an Array element
+                    Ex. String --> "Banana || Apple"
+
+                        Array w/ delimitter/cut off --> {"Banana", "Apple"}
+                */
+                String[] file_line = line.split("\\|\\|"); // "\\||" - Literally takes only "||"
+                
+                for (int i = 0; i<file_line.length; i++)
+                {
+                    file_line[i] = file_line[i].replace("<br>","\n"); //Replaces each line break with a new line ASNI character.
+                    System.out.print(file_line[i]); //Prints for tetsing
+                }
+
+            }
+            reader.close(); //Closes File
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File Not Found..."); //Error Message
+            e.printStackTrace();
+        }
 
         input.close(); //Closes Input, to prevent Memory Leak
+
     }
     
 }
